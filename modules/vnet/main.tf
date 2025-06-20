6687d2a631380b7907a97e1e8074a1efac6297f5
tahmidur22 <tahmidur55@gmail.com>
2025-06-20 19:46:54 +0100

    clean up

---
+++ modules/vnet/main.tf
@@ -0,0 +1,1 @@
+//Vnet config for Main.tf
---
+++ outputs.tf
@@ -0,0 +1,6 @@
+output "mssql_server_name" {
+  value = module.database.server_name
+}
+output "database_name" {
+  value = module.database.database_name
+}
