824b1dbf916086638e412a0d9cb5d17b764e0c17
tahmidur22 <tahmidur55@gmail.com>
2025-06-21 23:31:58 +0100

    Removed module and clean up

--- main.tf
+++ main.tf
@@ -1,12 +1,13 @@
+resource "azurerm_resource_group" "arg" {
+  name     = var.resource_group_name
+  location = var.resource_location
+}
+
+
 module "database" {
   source              = "./modules/database"
   server_name         = var.mssql_server_name
-  resource_group_name = module.resource_group.name
-  resource_location   = module.resource_group.location
+  resource_group_name = azurerm_resource_group.arg.name
+  resource_location   = azurerm_resource_group.arg.location
   database_name       = var.database_name
-}
-module "resource_group" {
-  source   = "./modules/resource_group"
-  name     = var.resource_group_name
-  location = var.resource_location
 }
--- modules/resource_group/variables.tf
+++
@@ -1,8 +0,0 @@
-variable "name" {
-    type = string
-    description = "The name of the resource group."
-}
-variable "location" {
-    type = string
-    description = "The location of the resource group."
-}
